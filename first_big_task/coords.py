faaa54d409c66b360642bd34410b1090434954c9
Alexeyokey <alex_v685@mail.ru>
2024-02-09 19:24:23 +0300

    ok

--- first_big_task/coords.py
+++ first_big_task/coords.py
@@ -63,6 +63,9 @@ if __name__ == "__main__":
     # coords = [coords[1], coords[0]]
     response = requests.get(f'https://static-maps.yandex.ru/1.x/?lang=ru_RU&ll={coords[1]},{coords[0]}&spn={spn[0]},{spn[1]}&l=sat')
     img = pygame.image.load(BytesIO(response.content))
+    f1 = pygame.font.Font(None, 36)
+    text1 = f1.render('Введите координаты в формате: \n55.713010, 37.660449', True,
+                      (255, 255, 255))
     while running:
         for event in pygame.event.get():
             screen2 = pygame.Surface(screen.get_size())
@@ -75,9 +78,11 @@ if __name__ == "__main__":
                 position += event.rel
             elif event.type == pygame.MOUSEBUTTONUP:
                 flag = 0
+
         screen2.fill(pygame.Color('black'))
         screen2.blit(img, (0,0))
         screen.blit(screen2, (0, 0))
+        screen.blit(text1, (0, 500))
         pygame.display.flip()
         clock.tick(fps)
 
